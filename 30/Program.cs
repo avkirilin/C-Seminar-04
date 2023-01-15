03409fe9622b6e9420a45b4a109b9c3a4f90ff88
avkirilin <nerosashok@mail.ru>
2023-01-15 21:12:08 +0300

    решена четвертая задача семинара 4

---
+++ 30/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+void FillArray(int[] znachenie)
+{
+    int lenght = znachenie.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        znachenie[index] = new Random().Next(0, 2);
+        index++;
+    }
+}
+void PrintArray(int[] chisla)
+{
+    int count = chisla.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{chisla[position]} ");
+        position++;
+    }
+}
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
