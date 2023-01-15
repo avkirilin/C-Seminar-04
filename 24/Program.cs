73f955a220d81c28067238709b926b207f828119
avkirilin <nerosashok@mail.ru>
2023-01-15 20:31:40 +0300

    решена вторая задача 4 семинара

---
+++ 24/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А.
+Console.WriteLine ("Введите любое положительное целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+for (int i = 0; i <= a; i++)
+{
+    summ = summ + i;
+}
+Console.WriteLine ($"Сумма чисел от 0 до {a} будет равна {summ}");
