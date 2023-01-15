b7f0338eb06dbb4a48fc4992dbf12541791614a8
avkirilin <nerosashok@mail.ru>
2023-01-15 20:38:56 +0300

    решена третья задача семинара 4

---
+++ 28/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine ("Введите любое положительное целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+int umnoj = 1;
+for (int i = 1; i <= a; i++)
+{
+    umnoj = umnoj * i;
+}
+Console.WriteLine ($"Произведение чисел от 1 до {a} будет равна {umnoj}");
